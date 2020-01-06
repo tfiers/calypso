a1f7144f037f9dfa110a9e5d019788071780979b
Tomas Fiers <tomas.fiers@gmail.com>
2020-01-06 22:03:58 +0100

    Add before/after images

--- calypso.js
+++ calypso.js
@@ -1,23 +1,20 @@
-
-const GCAL_BLACK = "rgb(60, 64, 67)"
-
 const DOMMutationListener = _.throttle(editEventsStyle, 200)
 const observer = new MutationObserver(DOMMutationListener)
 observer.observe(document, { childList: true, subtree: true })
 
 function editEventsStyle() {
     if (inMonthView()) {
-        document.querySelectorAll('div[data-eventchip]>div[role="button"]')
-        .forEach(eventElement => {
-            const circleElement = eventElement.firstChild.querySelector("div[style]")
-            if (circleElement != null) {
-                circleElement.parentElement.style.display = "none"
-                const circleColor = circleElement.style.borderColor
-                eventElement.style.backgroundColor = circleColor
-                // Change black text color to white.
+        const eventElements = document.querySelectorAll('div[data-eventchip]>div[role="button"]')
+        Array.from(eventElements).forEach(element => {
+            const bgColor = element.style.backgroundColor
+            if (bgColor != "" && bgColor != "white") {
+                element.style.borderWidth = "1.5px"
+                element.style.borderColor = bgColor
+                element.style.backgroundColor = "white"
+                // Change white text color to black.
                 // (This leaves past events with greyed out text as is).
-                if (getComputedStyle(eventElement).color == GCAL_BLACK) {
-                    eventElement.style.color = "white"
+                if (getComputedStyle(element).color == "rgb(255, 255, 255)") {
+                    element.style.color = "black"
                 }
             }
         })
